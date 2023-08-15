4626fe31783db254e3b3d718686885123870083c
ARandomPenguin <103593185+ARandomPenguin@users.noreply.github.com>
2023-08-15 00:53:35 +0000

    Changes: Should Work (Hopefully)

--- main.py
+++ main.py
@@ -1,69 +1,85 @@
 import sys
-import csv 
+import csv
 import os
 import requests
+import re
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
-from datetime import date, datetime
-
-
+from datetime import date, datetime, timedelta
 
 
 def main():
+    # Read files
     class_schedule = read_class_schedule("class_schedule.csv")
-    sender_address, sender_password = get_sender_details("sender_details.txt")
-    current_day_of_week = date.today().strftime("%A")#Finds the current day of the week
-    current_time = datetime.now().time()#Finds the current time
-   
+    sender_address = get_sender_details("sender_details.txt")
+
+    # Gets the current day of the week
+    current_day_of_week = date.today().strftime("%A")
+
+    # Gets the current time
+    current_time = datetime.now().time()
+
     for class_info in class_schedule:
-            if class_info['Day'] == current_day_of_week and is_time_to_send(current_time, class_info['Time']):
-                send_class_reminders(class_info, sender_address)
+        # Get the day from Item
+        item = class_info['Item']
+        day_index = item.find(current_day_of_week)
+        day = item[day_index:item.find(' ', day_index + 1)]
 
-def is_time_to_send(current_time, class_time_str):
-    class_time = datetime.strptime(class_time_str.split(' ')[-2], "%I:%M %p").time()
-    time_difference = datetime.combine(date.today(), class_time) - datetime.combine(date.today(), current_time)
-    return 0 <= time_difference.total_seconds() < 3600  # Check if the class time is within 1 hour from now
+        if day and is_time_to_send(current_time, item):
+            send_class_reminders(class_info, sender_address)
 
 
+# Extracting the email of the recipient
 def send_class_reminders(class_info, sender_address):
     recipients = class_info['Email'].split(';')
+    item = class_info["Item"]
+    start_time, end_time, am_pm = re.findall(r'(\d:\d\d) - (\d:\d\d) ?(\w\w)', item)[0]
+    class_name = item[0:item.find(date.today().strftime("%A"))]
 
     for recipient in recipients:
         message = Mail(
             from_email=sender_address,
             to_emails=recipient,
-            subject=f'AEP Class Reminder: {class_info["Item"]} Today',
-            html_content=f'Hello {class_info["First Name"]},<br>'
-                         f'Your class "{class_info["Item"]}" is scheduled for today at "{class_info["Time"]}".<br>'
-                         f'Please remember to attend at the specified time.')
+            subject=f'AEP Class Reminder',
+            html_content=f"""
+                        Hello {class_info["First Name"]} {class_info["Last Name"]},<br><br>
+                        Your class {class_name} is scheduled for today at {start_time} - {end_time} {am_pm}.<br>
+                        Please remember to join at the specified time.
+                        """
+        )
 
         try:
             sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
             response = sg.send(message)
-            print(response.status_code)
-            print(response.body)
-            print(response.headers)
-             
+
             # Capture recipient's email address
             sent_to = recipient
 
             # Create a message with the recipient's information
-            discord_message = f"Email Sent!\nName: {class_info['First Name']} {class_info['Last Name']}}}Recipient: {sent_to}\nClass: {class_info['Item']}\nTime: {class_info['Time']}"
+            discord_message = f"""
+            Email Sent!
+            Name: {class_info['First Name']} {class_info['Last Name']}
+            Recipient: {sent_to}
+            Class: {class_name}
+            Time: {start_time} - {end_time} {am_pm}\n
+            """
 
             # Send the message to Discord webhook
             webhook_url = "https://discord.com/api/webhooks/1087912305876008960/3aRKWpauzvJrhSsRL2sLTuXM8TNnv5TlaFyiqGtGKv25G2cxWsOregwsyA9hUTWAcyr5"
             data = {
                 "content": discord_message
             }
-            response = requests.post(webhook_url, json=data)
-            if response.status_code != 204:
-                print(f"Error sending to Discord webhook. Status Code: {response.status_code}")
-        
+            response_discord = requests.post(webhook_url, json=data)
+
+            # Debugging purposes
+            if response_discord.status_code != 204:
+                print(f"Error sending to Discord webhook. Status Code: {response_discord.status_code}")
+                print(response_discord.content)
+
         except Exception as e:
-            print(str(e))
+            print(e)
 
 
-            
 def read_class_schedule(filename):
     class_schedule = []
     with open(filename, 'r', newline='', encoding='utf-8') as csv_file:
@@ -72,16 +88,25 @@ def read_class_schedule(filename):
             class_schedule.append(row)
     return class_schedule
 
+
 def get_sender_details(f):
     """
-    Gets the detials about the sender from f
+    Gets the details about the sender from f
     """
     try:
         with open(f, 'r') as f:
-            return f.read().split()
+            return f.read().split()[0]
 
     except FileNotFoundError:
         sys.exit(f"{f} Not Found.")
+
+
+def is_time_to_send(current_time, class_item):
+    start_time, end_time, am_pm = re.findall(r'(\d:\d\d) - (\d:\d\d) ?(\w\w)', class_item)[0]
+    class_start_time_str = f"{start_time} {am_pm}"
+    class_start_time = datetime.strptime(class_start_time_str, "%I:%M %p")
+    reminder_time = class_start_time - timedelta(hours=1)
+    return current_time >= reminder_time.time()
 
 
 if __name__ == '__main__':
