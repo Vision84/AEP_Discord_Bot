25afd362e96999c9a495dc4de2b9111fdea91733
Kumar-Samyak <113846541+Kumar-Samyak@users.noreply.github.com>
2023-08-25 20:48:58 -0500

    New csv files

--- AEP_discord_bot.py
+++ AEP_discord_bot.py
@@ -1,6 +1,5 @@
-# bot.py
 import os
-import json
+from tabulate import tabulate
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -23,32 +22,39 @@ async def on_ready():
 
 @bot.slash_command(name='greet', description='Greets You!')
 async def greet(ctx):
-    return await ctx.send(f'Hello {ctx.author.nick}!')
+    await ctx.respond(f'Hello {ctx.author.display_name}!')
 
 
 @bot.slash_command(name='send_reminders', description='Send Reminders to Students that have a class today.')
-
 async def send_reminders(ctx):
-    await ctx.send("Sending Reminders!")
-    # messages = main()
+    await ctx.respond("Sending Reminders!")
+    messages = main()
+    messages = [{1: '1,2,3,44,5,5,6'}, {2: '11,12,13,144,15,15,16'}]
+    table = tabulate(messages, headers='keys', tablefmt='heavy_grid', stralign='center')
+    print(table)
 
-    # for message in messages:
-        # await ctx.send(message)
+    for line in table.splitlines():
+     await ctx.send(line)
+
+    # for subject, student  in messages.items():
+    #     await ctx.send(subject)
+    #     await ctx.send(student)
+    #     await ctx.send('================================================================')
 
 
-@bot.slash_command(name='send_reminder_to', description='Send Reminder to a specific student.')
+# @bot.slash_command(name='send_reminder_to', description='Send Reminder to a specific student.')
 
-async def send_reminder_to(ctx, student_fname, student_lname):
-    student_name = (student_fname.strip() + ' ' + student_lname.strip()).title()
-    class_schedule = read_class_schedule("main.csv")
-    sender_address = get_sender_details("sender_details.txt")
+# async def send_reminder_to(ctx, student_fname, student_lname):
+#     student_name = (student_fname.strip() + ' ' + student_lname.strip()).title()
+#     class_schedule = read_class_schedule("main.csv")
+#     sender_address = "academicempowermentproject@gmail.com"
 
-    for student in class_schedule:
-        current_name = student['First Name'] + ' ' + student['Last Name']
-        current_name = current_name.title()
-        if student_name == current_name:
-            await ctx.send(send_class_reminders(student, sender_address))
-            return
+#     for student in class_schedule:
+#         current_name = student['First Name'] + ' ' + student['Last Name']
+#         current_name = current_name.title()
+#         if student_name == current_name:
+#             await ctx.send(send_class_reminders(student, sender_address))
+#             return
 
 
 bot.run(TOKEN)
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sys
 import csv
 import os
-import requests
 import re
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
@@ -10,49 +8,58 @@ import pytz
 
 
 def main():
-    # If new data, then filter it
-    # filter_csv("SignUpGenius (1).csv", "class_schedule.csv")
 
     # Read files
-    class_schedule = read_class_schedule("class_schedule.csv")
+    # class_schedule = read_class_schedule("class_schedules/")
     # class_schedule = read_class_schedule("test.csv")
-    sender_address = get_sender_details("sender_details.txt")
+    sender_address = "academicempowermentproject@gmail.com"
 
     # Gets the current day of the week
     dt_us_central = datetime.now(pytz.timezone('US/Central'))
     current_day_of_week = dt_us_central.strftime("%A")
 
-    messages = []
+    # path = 'class_schedules/'
+    path = 'test/'
+    files = os.listdir(path)
 
-    for class_info in class_schedule:
-        # Get the day from Item
-        item = class_info['Item']
-        day_index = item.find(current_day_of_week)
-        day = item[day_index:item.find(' ', day_index + 1)]
+    messages = {}
+    
+    for file in files:
+        message = []
 
-        if day:
-            messages.append(send_class_reminders(class_info, sender_address))
+        class_schedule = read_class_schedule(path, file)
+
+        for class_info in class_schedule:
+            day = class_info['Day']
+
+            if day == current_day_of_week:
+                message.append(send_class_reminders(class_info, sender_address))
+        
+        messages[file] = message
 
     return messages
 
 
-# Extracting the email of the recipient
+# Send email to student
 def send_class_reminders(class_info, sender_address):
+    fname = class_info["Student First Name"].strip().capitalize()
+    lname = class_info["Student Last Name"].strip().capitalize()
     recipient = class_info['Email']
-    item = class_info["Item"]
-    start_time, end_time, am_pm = re.findall(r'(\d\d?:\d\d) - (\d\d?:\d\d) ?(\w\w)', item)[0]
-    class_name = item[0:item.find(date.today().strftime("%A"))]
+    start_time = class_info['Start Time'].strip()
+    end_time = class_info['End Time'].strip()
+    subject = class_info['Subject'].strip()
+    teacher = class_info['Teacher'].strip().title()
     
     message = Mail(
         from_email=sender_address,
         to_emails=recipient,
         subject='AEP Class Reminder',
         html_content=f"""
-                    Hello {class_info["First Name"].strip().capitalize()} {class_info["Last Name"].strip().capitalize()},<br><br>
-                    Your class, {class_name.strip().title()}, is scheduled for today at {start_time.strip()} - {end_time.strip()} {am_pm.lower().strip()}.<br>
+                    Hello {fname} {lname},<br><br>
+                    Your class, {subject}, is scheduled for today at {start_time} - {end_time}. Your teacher is {teacher}.<br>
                     Please remember to join at the specified time.
                     <br><br>
-                    Check the google classroom for the link to the meeting.
+                    Check the google classroom for the link to the meeting.<br>
                     Google Classrrom: https://classroom.google.com/c/NjE2OTI0NjAyNjQ3
                     """
     )
@@ -62,79 +69,29 @@ def send_class_reminders(class_info, sender_address):
         response = sg.send(message)
         print(response.status_code)
 
-        # Capture recipient's email address
-        sent_to = recipient
-
         # Create a message with the recipient's information
         discord_message = f"""
         Email Sent!
-        Name: {class_info['First Name']} {class_info['Last Name']}
-        Recipient: {sent_to}
-        Class: {class_name}
-        Time: {start_time} - {end_time} {am_pm}\n
+        Name: {fname} {lname}
+        Email: {recipient}
+        Class: {subject}
+        Time: {start_time} - {end_time}
+        Teacher: {teacher}\n
         """
 
         return discord_message
-
-        """
-        # Send the message to Discord webhook
-        webhook_url = "https://discord.com/api/webhooks/1087912305876008960/3aRKWpauzvJrhSsRL2sLTuXM8TNnv5TlaFyiqGtGKv25G2cxWsOregwsyA9hUTWAcyr5"
-        data = {
-            "content": discord_message
-        }
-        response_discord = requests.post(webhook_url, json=data)
-
-        # Debugging purposes
-        if response_discord.status_code != 204:
-            print(f"Error sending to Discord webhook. Status Code: {response_discord.status_code}")
-            print(response_discord.content)
-        """
 
     except Exception as e:
         print(e)
 
 
-def read_class_schedule(filename):
+def read_class_schedule(path, filename):
     class_schedule = []
-    with open(filename, 'r', newline='', encoding='utf-8') as csv_file:
+    with open(path + filename, 'r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
-        capitalize_words = ['First Name', 'Last name']
         for row in csv_reader:
-            row = {k:v.title() if k in capitalize_words else v for k, v in row.items()}
             class_schedule.append(row)
     return class_schedule
-
-
-def get_sender_details(f):
-    """
-    Gets the details about the sender from f
-    """
-    try:
-        with open(f, 'r') as f:
-            return f.read()
-
-    except FileNotFoundError:
-        sys.exit(f"{f} Not Found.")
-
-
-def filter_csv(in_file, out_file):
-    data = []
-    required_data = ['Item', 'First Name', 'Last Name', 'Email']
-
-    try:
-        with open(in_file, 'r') as f:
-            reader = csv.DictReader(f)
-            for row in reader:
-                updated_row = {k:v for k, v in row.items() if k in required_data}
-                data.append(updated_row)
-
-        with open(out_file, 'w') as f:
-            writer = csv.DictWriter(f, fieldnames=required_data)
-            writer.writeheader()
-            writer.writerows(data)
-
-    except FileNotFoundError:
-        sys.exit(f"{in_file} Not Found.")
 
 
 if __name__ == '__main__':
