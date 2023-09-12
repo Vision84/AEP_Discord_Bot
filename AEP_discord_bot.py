10977165a00d173c1027095ba70f6b8fcfe382a8
Kumar-Samyak <113846541+Kumar-Samyak@users.noreply.github.com>
2023-09-11 19:49:16 -0500

    Completed /list_students (Design is poor, as of now)

--- AEP_discord_bot.py
+++ AEP_discord_bot.py
@@ -38,19 +38,19 @@ async def send_reminders(ctx):
         await ctx.send('================================================================')
 
 
-@bot.slash_command(name='list_students', description='List the students in a class OR a teacher.')
+@bot.slash_command(name='list_students', description='List the students in a class OR a teacher OR both.')
 async def list_students(ctx, class_name: str = None, teacher: str = None):
     
     # If only class name is provided
     if class_name and not teacher:
         for file in files:
             class_schedule = read_class_schedule(path, file)
-            list_to_return = {}
+            to_return = {}
             students = []
             current_teacher = ""
             flag = False
             for c in class_schedule:
-                if c['Subject'] != class_name: continue
+                if c['Subject'].lower() != class_name.lower(): continue
                 
                 flag = True
                 if current_teacher != c['Teacher']: students = []
@@ -58,18 +58,70 @@ async def list_students(ctx, class_name: str = None, teacher: str = None):
 
                 d = {'Day': c['Day'], 'Time': c['Start Time'] + " - " + c['End Time'], 'Students': students}
                 students.append(c['Student First Name'] + " " + c["Student Last Name"])
-                list_to_return[c['Teacher']] = d
+                to_return[c['Teacher']] = d
 
             if flag: break
             
         await ctx.respond("# " + class_name)
-        for teacher_name, contents in list_to_return.items():
+        for teacher_name, contents in to_return.items():
             await ctx.send("## " + teacher_name + " (" + contents['Day'] + " " + contents['Time'] + ")")
             for student in contents['Students']:
                 await ctx.send("- " + student)
             
+    # If only teacher name is provided
+    elif teacher and not class_name:
+        to_return = {}
+        
+        for file in files:
+            class_schedule = read_class_schedule(path, file)
+            current_subject = ""
+            students = []
+            for c in class_schedule:
+                if c['Teacher'].lower() != teacher.lower(): continue
 
+                if current_subject != c['Subject']: students = []
+                current_subject = c['Subject']
+                
+                students.append(c['Student First Name'] + " " + c["Student Last Name"])
+                d = {"Day": c['Day'], 'Time': c['Start Time'] + " - " + c['End Time'], 'Students': students}
+                to_return[c['Subject']] = d
 
+        await ctx.respond("# " + teacher)
+        for subject, contents in to_return.items():
+            await ctx.send("## " + subject + " (" + contents['Day'] + " " + contents['Time'] + ")")
+            for student in contents['Students']:
+                await ctx.send("- " + student)
+
+    # If both class and teacher is provided
+    elif class_name and teacher:
+        for file in files:
+            class_schedule = read_class_schedule(path, file)
+            to_return = {}
+            students = []
+            flag = False
+
+            for c in class_schedule:
+                if c['Subject'].lower() != class_name.lower() or c['Teacher'].lower() != teacher.lower(): continue
+
+                flag = True
+
+                to_return['Day'] = c['Day']
+                to_return['Time'] = c['Start Time'] + " - " + c['End Time']
+
+                students.append(c['Student First Name'] + " " + c["Student Last Name"])
+                to_return['Students'] = students
+
+            if flag: break
+
+        await ctx.respond("# " + class_name + " by " + teacher)
+        await ctx.send("## Day: " + to_return['Day'])
+        await ctx.send("## Time: " + to_return['Time'])
+        for student in to_return['Students']:
+            await ctx.send("- " + student)
+
+    # If none
+    else:
+        ctx.respond("Provide a class name or a teacher's name or both.")
 
 
 @bot.slash_command(name='send_reminder_to', description='Send Reminder to a specific student.')
